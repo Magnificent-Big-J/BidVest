3f47f5daee50c4c9e67f99dbc8b3f1fc5ab3bdaf
Joel Mnisi <joel.mnisi@protonmail.com>
2020-07-17 12:18:38 +0200

    Fixing logic

--- run.php
+++ run.php
@@ -3,12 +3,11 @@
 require_once('classes/StudentInterface.php');
 require_once('classes/StudentAbstract.php');
 require_once('classes/Student.php');
-
-if (!empty($argv)) {
-    //Remove file name
-     unset($argv[0]);
-     //Reset array index
-     $arguments = array_merge($argv);
+//Remove file name
+unset($argv[0]);
+//Reset array index
+$arguments = array_merge($argv);
+if (!empty($argv)) {   
 
      //Instatiate student object
      $student = new Student();    
